ebfd5b7c5c85bdb1e0e414190de43519eca57d4c
Bartek Wasielak <bartek@wasielak.eu>
2013-07-28 20:42:34 +0900

    basic version

---
+++ CountDown/js/events.js
@@ -0,0 +1,58 @@
+﻿(function () {  
+    var Event = WinJS.Class.define(
+        function (name, date) {
+            this._initObservable();
+            this.name = name;
+            this.date = date;
+            this.mainSpanValue = "";
+            this.secondarySpan = "";
+            this.mainSpanUnit = "";
+        },
+        {
+            _calculate: function() {
+                var date = Date.parse(this.date);
+                var diff = date - new Date();
+                var days = Math.floor(diff / Event._d);
+                var hours = Math.floor((diff - (days * Event._d)) / Event._h);
+                var minutes = Math.floor((diff - (days * Event._d + hours * Event._h)) / Event._m);
+                var seconds = Math.floor((diff - (days * Event._d + hours * Event._h + minutes * Event._m)) / Event._s);
+                if (days != 0) {
+                    this.setProperty("mainSpanValue", days);
+                    this.setProperty("mainSpanUnit", "days");
+                    this.setProperty("secondarySpan", hours + " hours " + minutes + " minutes " + seconds);
+                }
+                if (hours != 0) {
+                    this.setProperty("mainSpanValue", hours);
+                    this.setProperty("mainSpanUnit", "hours");
+                    this.setProperty("secondarySpan", minutes + " minutes " + seconds);
+                }
+                this.setProperty("mainSpanValue", minutes);
+                this.setProperty("mainSpanUnit", "minutes");
+                this.setProperty("secondarySpan", seconds);
+            }
+        },
+        {
+            _d: 86400000,
+            _h: 3600000,
+            _m: 60000,
+            _s: 1000
+        }
+        );
+    
+    WinJS.Class.mix(Event,
+        WinJS.Binding.mixin,
+        WinJS.Binding.expandProperties({ name: "", mainSpanUnit: "", mainSpanValue: "", secondarySpan: "" })
+    );
+    
+    WinJS.Namespace.define("Events", {
+        Event: Event,
+
+        model: WinJS.Binding.as({
+            date: new Date(),
+            events: new WinJS.Binding.List([
+                new Event("Koniec projektu", "2013-08-09T17:00"),
+                new Event("Przylot Izy", "2013-08-11T06:25")
+            ])
+        })
+    });
+})();
---
+++ Pluralize/PluralizeExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralize
+{
+    public static class PluralizeExtension
+    {
+        public static string Pluralize(this string pluralizeBase, int number)
+        {
+            return PluralizationProvider.Pluralize(pluralizeBase, number);
+        }
+    }
+}
